f626682b867ec016ba0bb8bc29a945d9bfc416ec
hansungwoo7308 <hansungwoo7308@gmail.com>
2023-09-15 19:04:50 +0900

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,25 @@
 "use client";
 import Link from "next/link";
 import "@/app/page.scss";
-import { motion } from "framer-motion";
+import { motion, useMotionValue, useMotionValueEvent, useScroll } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [hidden, setHidden]: any = useState(false);
+  const { scrollY } = useScroll();
+  // MotionValueEvent가 scrollY의 값을 트랙킹(추적)한다.
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const previous = scrollY.getPrevious();
+    console.log({ previous, latest });
+    // scroll down : previous < latest
+    if (previous < latest && 100 < latest) setHidden(true);
+    // scroll up : previous > latest
+    else setHidden(false);
+  });
+  // useEffect(() => {
+  //   const unsubscribe = scrollY.on("change", (latestValue: number) => console.log({ latestValue }));
+  //   return () => unsubscribe();
+  // }, []);
   return (
     <header>
       <section className="section">
@@ -13,8 +29,10 @@ export default function Header() {
             visible: { y: 0 },
           }}
           initial="hidden"
-          animate="visible"
+          animate={hidden ? "hidden" : "visible"}
+          // animate="visible"
           transition={{ duration: 0.5 }}
+          // scroll
         >
           <Link href={"/#home"}>Home</Link>
           <Link href={"/#about"}>About</Link>
