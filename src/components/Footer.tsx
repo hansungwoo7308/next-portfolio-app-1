01756e5320b89dd2aacffdf2e3cf6dd732e4715e
hansungwoo7308 <hansungwoo7308@gmail.com>
2023-09-21 18:09:29 +0900

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { SocialIcon } from "react-social-icons";
 const size = { width: "2rem", height: "2rem" };
 export default function Footer() {
@@ -7,18 +8,26 @@ export default function Footer() {
         <div className="left">
           <h1 className="title">Youser Stack</h1>
           <div className="social">
-            <SocialIcon url="www.github.com" href="/#" style={size} />
-            <SocialIcon url="www.twitter.com" href="/#" style={size} />
-            <SocialIcon url="www.kakao.com" href="/#" style={size} />
+            <SocialIcon className="icon" url="www.github.com" href="/#" style={size} />
+            <SocialIcon className="icon" url="www.twitter.com" href="/#" style={size} />
+            <SocialIcon className="icon" url="www.kakao.com" href="/#" style={size} />
           </div>
         </div>
         <div className="right">
           <h1>Index</h1>
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Works</li>
-            <li>Contact</li>
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/about"}>About</Link>
+            </li>
+            <li>
+              <Link href={"/works"}>Works</Link>
+            </li>
+            <li>
+              <Link href={"/contact"}>Contact</Link>
+            </li>
           </ul>
         </div>
       </section>
